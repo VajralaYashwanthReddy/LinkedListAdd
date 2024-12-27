a7898dc7b662ac0866f7e3f257fce5b2ed45a357
YashwanthReddy2004 <2311cs010669@mallareddyuniversity.ac.in>
2024-12-27 13:53:32 +0530

    Create LinkedListAdd.java

---
+++ LinkedListAdd.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+public class LinkedListAdd{
+	public static void main(String[] args) {
+	    //LinkedList ADD
+	    LinkedList LL = new LinkedList();
+	    LL.add("Benz");
+	    LL.add("Valvo");
+	    LL.add("XUV 500");
+		System.out.println(LL);
+	}
+}
